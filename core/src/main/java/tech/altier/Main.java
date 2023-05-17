c97ed1a9f051f24260ba762cbf217a1f2fd16e68
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-17 19:06:33 +0530

    Update Main.java

--- core/src/main/java/tech/altier/Main.java
+++ core/src/main/java/tech/altier/Main.java
@@ -1,11 +1,9 @@
 package tech.altier;
 
-import com.pkslow.ai.*;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.Duration;
+
 import java.util.Scanner;
 
 public class Main {
