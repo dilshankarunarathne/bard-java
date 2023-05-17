b8b360418bb48b92a79027afe8cd8420c57bfcd6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-17 19:06:17 +0530

    Update Main.java

--- core/src/main/java/tech/altier/Main.java
+++ core/src/main/java/tech/altier/Main.java
@@ -6,13 +6,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Duration;
+import java.util.Scanner;
 
 public class Main {
     private static final String token = "e01a29d7-8a2d-4af6-9784-f8519d364209";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         while (true) {
-            
+            System.out.print("You: ");
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
+            try {
+                String response = curl(input);
+                System.out.println("Bard: " + response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
