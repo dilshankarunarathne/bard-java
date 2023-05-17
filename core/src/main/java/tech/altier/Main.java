f9a5041706e1fa710ecb6af11c97a6ac953c59e9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-17 18:57:40 +0530

    Update Main.java

--- core/src/main/java/tech/altier/Main.java
+++ core/src/main/java/tech/altier/Main.java
@@ -1,7 +1,31 @@
 package tech.altier;
 
+import com.pkslow.ai.*;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
-        
+        NetworkUtils.setUpProxy("localhost", "7890");
+        String token = args[0];
+        AIClient client = new GoogleBardClient(token, Duration.ofMinutes(10));
+
+        Answer answer = client.ask("What is the population of London?");
+        printChosenAnswer(answer);
+
+        answer = client.ask("How about Beijing?");
+        printChosenAnswer(answer);
+
+        answer = client.ask("How about Hong Kong?");
+        printChosenAnswer(answer);
+    }
+
+    private static void printChosenAnswer(Answer answer) {
+        StringBuilder sb = new StringBuilder();
+        if (answer.status() == AnswerStatus.OK) {
+            sb.append("\n### Chosen Answer\n");
+            sb.append(answer.chosenAnswer());
+            System.out.println("Output: \n " + sb);
+        }
     }
 }
