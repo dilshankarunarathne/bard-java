bcc83e7c968ecba944d01678dc80ec81d6c251a7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-17 19:42:21 +0530

    Update Main.java

--- core/src/main/java/tech/altier/Main.java
+++ core/src/main/java/tech/altier/Main.java
@@ -1,55 +1,47 @@
 package tech.altier;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 
-public class Main {
-    private static final String token = "e01a29d7-8a2d-4af6-9784-f8519d364209";
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
+class Main {
+    public static void main(String[] args) throws IOException {
         while (true) {
+            Scanner scanner = new Scanner(System.in);
             System.out.print("You: ");
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            }
-            try {
-                String response = curl(input);
-                System.out.println("Bard: " + response);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            if (input.equals("exit")) break;
+            System.out.println("Bot: " + chat(input));
         }
+    }
+
+    private static parseResponse(String response) {
+        
     }
 
     private static String chat(String input) throws IOException {
-        String Demo_Command = "curl -X POST https://localhost/post --data delft1=stack1&delft2=stack2";
-        Process Demo_process = Runtime.getRuntime().exec(Demo_Command);
-        InputStream Input_Stream = Demo_process.getInputStream();
-        Demo_process.destroy();
-    }
+        URL url = new URL("https://api.bardapi.dev/chat");
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("POST");
 
-    private static String curl(String input) throws IOException {
-        String[] commands = {
-                "curl", "-X", "POST", "https://api.bardapi.dev/chat",
-                "-H", "Authorization: Bearer " + token,
-                "-H", "Content-Type: text/plain",
-                "-d", "{\"input\":\"" + input + "\"}"
-        };
-        Process process = Runtime.getRuntime().exec(commands);
-        BufferedReader reader = new BufferedReader(new
-                InputStreamReader(process.getInputStream()));
-        String line;
-        String response = "";
-        while ((line = reader.readLine()) != null) {
-            response += line;
-        }
+        httpConn.setRequestProperty("Authorization", "Bearer e01a29d7-8a2d-4af6-9784-f8519d364209");
+        httpConn.setRequestProperty("Content-Type", "text/plain");
 
+        httpConn.setDoOutput(true);
+        OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
+        writer.write("{\"input\": \"" + input + "\"}");
+        writer.flush();
+        writer.close();
+        httpConn.getOutputStream().close();
+
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
+        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+        String response = s.hasNext() ? s.next() : "";
         return response;
     }
 }
